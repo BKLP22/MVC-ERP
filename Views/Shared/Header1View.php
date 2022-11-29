1829c857a451847eb421ac5055e547768857ab51
StevenMPG <stmiguel156@gmail.com>
2022-11-29 10:19:59 +0100

    cambioHeader

--- Views/Shared/Header1View.php
+++ Views/Shared/Header1View.php
@@ -1,28 +1,28 @@
     <header class="flex">
-        <div class="col-2">
-            <h1 class="erp pl-2">ERP</h1>
+        <div class="col-2  center">
+            <a class="erpa" href="#">ERP</a>
         </div>
-        <div class="col-4 flex">
-            <div class="botones">
-                <a href="iniciarsesion.php" class="enlaces">Inicio</a>
+        <div class="col-8 center">
+            <div class="botones col-4">
+                <a href="iniciarsesion.php" class="enlaces ahover">Inicio</a>
             </div>
-           <div class="botones">
-                <a href="registrarse.php" class="enlaces">Nosotros</a>
+           <div class="botones col-4">
+                <a href="registrarse.php" class="enlaces ahover">Nosotros</a>
            </div> 
-           <div class="botones">
-                <a href="#" class="enlaces">Contactos</a>
+           <div class="botones col-4">
+                <a href="#" class="enlaces ahover">Contactos</a>
            </div> 
         </div>
-        <form class="divlupa col-4" id="formConsulta1">
+        
+        <!-- <form class="divlupa col-4" id="formConsulta1">
             
                 <div><input type="text" name="" id="campotexto1" class="pl">
                 <input type="image" value=""  src="Assets/img/lupa.png" id="botonConsulta1" class="buscar"></input>
                 </div>
                 
-        </form>
+        </form> -->
         <div class="col-2" id="usuario">
-            <a href="iniciarsesion.php"><img src="Assets/img/usuario.png" alt="" class="erp"></a>
-            
+            <a class="erp" href="iniciarsesion.php"><img  src="Assets/img/usuario.png" alt="" ></a>
         </div>
         <div>
 
