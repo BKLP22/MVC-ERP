f26886e68ca59673e8811ec240eeef008157f39e
StevenMPG <stmiguel156@gmail.com>
2022-12-01 13:21:33 +0100

    cambioo de la nav y botones

--- Views/Shared/Header1View.php
+++ Views/Shared/Header1View.php
@@ -1,4 +1,4 @@
-    <header class="flex">
+    <!-- <header class="flex">
         <div class="col-2  center">
             <a class="erpa" href="#">ERP</a>
         </div>
@@ -14,17 +14,35 @@
            </div> 
         </div>
         
-        <!-- <form class="divlupa col-4" id="formConsulta1">
+        <form class="divlupa col-4" id="formConsulta1">
             
                 <div><input type="text" name="" id="campotexto1" class="pl">
                 <input type="image" value=""  src="Assets/img/lupa.png" id="botonConsulta1" class="buscar"></input>
                 </div>
                 
-        </form> -->
+        </form>
         <div class="col-2" id="usuario">
             <a class="erp" href="iniciarsesion.php"><img  src="Assets/img/usuario.png" alt="" ></a>
         </div>
         <div>
 
         </div>
+    </header> -->
+    <header class="flex">
+        <div class="col-2">
+            <a href=""><img src="./img/pngtree-bulb-vector-icon-white-transparent-background-png-image_1978020.png" alt="icono"></a>
+        </div>
+        <div class="col-8 flex cosa">
+            <a class="botonnav col-3" href="iniciarsesion.php">Inicio</a>
+            <a class="botonnav col-3" href="registrarse.php">Nosotros</a>
+            <a class="botonnav col-3" href="">Inserciones</a>
+            <a class="botonnav col-3" href="">Registros</a>
+        </div>
+        <div class="col-2 flex">
+            <div class="col-7"> <a class="" href="registrarse.php"><img src="./img/acceso.png" alt="registro"></a></div>
+           <div class="col-5"> <a class="" href="iniciarsesion.php"><img src="./img/acceso.png" alt="iniciosesion"></a></div>
+           
+        </div>
     </header>
+    
+    
